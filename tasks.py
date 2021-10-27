ed7e238232d1bd430cf70ffdb5189a91d6d27001
Johannes Marian Landmann <j_landmann@gmx.de>
2021-10-27 13:33:53 +0200

    server shenanigans

--- tasks.py
+++ tasks.py
@@ -4,6 +4,7 @@ import time
 import logging
 import datetime as dt
 import urllib.request
+from urllib.error import HTTPError
 import visualization
 import pandas as pd
 
@@ -15,7 +16,7 @@ logging.basicConfig(format='%(asctime)s: %(name)s: %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S', level=logging.DEBUG)
 # Module logger
 log = logging.getLogger(__name__)
-log.setLevel('DEBUG')
+log.setLevel('INFO')
 
 
 # parse settings and credentials
@@ -41,16 +42,19 @@ def retrieve_last(station_list):
             json_df = pd.read_json(urllib.request.urlopen(api_url).read(),
                                    typ='series')
             if 'error' in json_df:  # No new images in interval
-                log.warning('Error for station {}: {}'.format(str(s),
+                print('Error for station {}: {}'.format(str(s),
                                                             json_df.error))
                 continue
             else:
                 try:
                     last_img_full = json_df['last_image'].iloc[0]
                 except KeyError:
-                    log.warning('Error for station {}'.format(str(s)))
+                    print('Error for station {}'.format(str(s)))
 
-        last_img_full = json_df['last_image'].iloc[0]
+        try:
+            last_img_full = json_df['last_image'].iloc[0]
+        except KeyError:  # last image older than 24 hrs
+            continue
 
         last_img_mod = dt.datetime.strptime(last_img_full, '%Y-%m-%d %H:%M:%S')
         time_str = last_img_mod.strftime('%H:%M')
@@ -66,12 +70,35 @@ def retrieve_last(station_list):
         try:
             urllib.request.urlretrieve(url, save_path, None)
             log.info('Successfully retrieved {}.'.format(url))
+
+        except HTTPError:
+            # 1 minute diff. between acquisition and arrival on Holfuy server
+            last_img_mod -= dt.timedelta(minutes=1)
+            time_str = last_img_mod.strftime('%H:%M')
+            url = holfuy_dyn_url.format(str(s), date_str, time_str)
+            log.info('Trying {}...'.format(url))
+            try:
+                urllib.request.urlretrieve(url, save_path, None)
+                log.info('Successfully retrieved {}.'.format(url))
+            except HTTPError:
+                log.warning('Image for station {} on {} could not be' 
+                             'retrieved'.format(str(s), time_str))
+
+            # set file permissions
+            try:
+                os.chmod(save_path, 0o2750)
+            except:
+                print('Setting permissions went wrong...')
         except Exception as e:
-            print(e)
-            log.warning(
+            log.error(e)
+            log.info(
                 'Image for station {} on {} could not be retrieved. Error '
                 'message: {}'.format(str(s), last_img_record_time.
                                      strftime('%Y-%m-%d %H:%M'), str(e)))
+        try:
+            os.chmod(save_path, 0o2750)
+        except:
+            print('Setting permissions went wrong....')
 
 
 def setup_tasks():
@@ -111,14 +138,24 @@ def daily_tasks():
 
     if cfg['copy_to_uni_fribourg']:
         for ufrs in cfg['stations_to_fribourg']:
-            utils.copy_to_ufr_ftp(os.path.join(cfg['save_dir'], str(ufrs)))
+            try:
+                utils.copy_to_ufr_ftp(os.path.join(cfg['save_dir'], str(ufrs)))
+            except:
+                log.warning('Copy to Fribourg failed....')
 
     ani_dir = os.path.join(cfg['save_dir'], 'animations')
     for s in cfg['stations']:
         ani_path = os.path.join(ani_dir, '{}.mp4'.format(s))
         if not os.path.exists(os.path.dirname(ani_path)):
             os.mkdir(os.path.dirname(ani_path))
-        # todo: put the film making and copying to webpage here
+        try: 
+            visualization.make_image_animation(path=os.path.join(cfg['save_dir'],
+                                                                 str(s)),
+                                               save_path=ani_path)
+        except ValueError:  # when no files there yet
+            pass
+            
+    utils.copy_to_webpage_dir(ani_dir) 
 
 
 if __name__ == '__main__':
